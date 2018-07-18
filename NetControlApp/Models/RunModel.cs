b50e3e4c9c264262492d91067c8c762a2c486e3a
NeginM <negin.majidi45@gmail.com>
2018-07-18 12:51:37 +0430

    add Runs Database

---
+++ NetControlApp/Models/RunModel.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace NetControlApp.Models
+{
+    public class RunModel
+    {
+        [Key]
+        public int RunId { get; set; }
+
+        [Required]
+        public ApplicationUser User { get; set; }
+
+        [Required]
+        public string RunName { get; set; }
+
+        [Required]
+        public DateTime Time { get; set; }
+
+        [Required]
+        public string NetType { get; set; }
+
+        [Required]
+        public string NetNodes { get; set; }
+
+        [Required]
+        public bool DoContanct { get; set; }
+
+        [Required]
+        public string Network { get; set; }
+
+        [Required]
+        public string Target { get; set; }
+
+        [Required]
+        public string DrugTarget { get; set; }
+
+        [Required]
+        public string AlgorithmType { get; set; }
+
+        [Required]
+        public string AlgorithmParams { get; set; }
+
+        [Required]
+        public double Progress { get; set; }
+
+        [Required]
+        public string BestResult { get; set; }
+
+        [Required]
+        public bool IsCompleted { get; set; }
+    }
+}
