21d8fb219897e7ee318c6e3d3d0b49d96db55821
Diana <idiana96@yahoo.com>
2018-07-20 14:35:05 +0300

    table styled for index

--- NetControlApp/Migrations/20180720094223_blabla.cs
+++ NetControlApp/Migrations/20180720094223_blabla.cs
@@ -4,7 +4,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 
 namespace NetControlApp.Migrations
 {
-    public partial class asd : Migration
+    public partial class blabla : Migration
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
