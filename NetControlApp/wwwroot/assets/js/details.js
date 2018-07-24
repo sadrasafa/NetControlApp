1a7051521f8c6dc0afee34aa426673afbc5ae633
Diana <idiana96@yahoo.com>
2018-07-24 19:02:15 +0300

    new ui

---
+++ NetControlApp/wwwroot/assets/js/details.js
@@ -0,0 +1,15 @@
+﻿$(document).ready(function () {
+    $('.asd, .dit').on('click', function () {
+        var clicked = $(this);
+            
+            //$(this.className + '> .collapseDiv').collapse("show");
+            $('.collapseDiv').collapse('hide');
+            $(this).find('.collapseDiv').collapse('show');
+        
+        
+    });
+    $('.icon').on('click', function (e) {
+        e.stopPropagation();
+    });
+
+});
